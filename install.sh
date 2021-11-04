ba76ccbb10f31f7b73fc77a46ff7e4f3f0459d4a
Jonas Manthey <jonas.manthey@psi.ch>
2021-11-04 23:01:30 +0100

    initial release

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# bash completion should be installed on all systems
+# it is required for the complete-alias
+wget https://raw.githubusercontent.com/cykerway/complete-alias/master/complete_alias -O ~/.complete-alias
+
+cp  aktentasche_aliases ~/.aktentasche_aliases
+echo "source ~/.aktentasche_aliases" >> ~/.bashrc
+
+cp  aktentasche_vimrc ~/.aktentasche_vimrc
